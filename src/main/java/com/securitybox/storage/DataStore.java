9c58dd6b10d28279d5d992cba102549a4aa9158f
Kavindra Kulathilake <kkulathilake@gmail.com>
2019-03-14 23:13:09 +0100

    Comments added...

--- src/main/java/com/securitybox/storage/DataStore.java
+++ src/main/java/com/securitybox/storage/DataStore.java
@@ -6,25 +6,23 @@ import org.apache.ignite.IgniteCache;
 import org.apache.ignite.Ignition;
 import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.transactions.TransactionException;
-
-import javax.transaction.TransactionRequiredException;
-
-import static java.lang.Enum.valueOf;
 import static org.apache.ignite.cache.CacheAtomicityMode.TRANSACTIONAL;
 
 public  class DataStore implements DataStoreDao{
     public static Ignite ignite;
     public static IgniteCache<String, String> cache;
+
+    //initiate a ignite cache with default settings to be used for storing token and values
     public DataStore(){
+        //initialize the ignite cache and start instance
         ignite = Ignition.start();
         CacheConfiguration cfg = new CacheConfiguration();
         cfg.setName(Constants.IGNITE_DEFAULT_CACHE_NAME);
         cfg.setAtomicityMode(TRANSACTIONAL);
         cache = ignite.getOrCreateCache(cfg);
-
-
     }
 
+    //function to store the key value pair, given a token together with the string value
     @Override
     public  boolean storeValue(String key, String value) {
         try {
@@ -35,13 +33,19 @@ public  class DataStore implements DataStoreDao{
         return true;
     }
 
+    //de-tokenize a given token
+    //NOTE : if token is not found in the cache, the same token value returned
     @Override
     public String retrieveValue(String token) {
+        String response="";
         try {
-            return cache.get(token);
+            response = cache.get(token);
+            if(response==null)
+                response=token;
         }catch(TransactionException e){
             System.out.println(e.getCause());
-            return null;
+            response = token;
         }
+        return response;
     }
 }
