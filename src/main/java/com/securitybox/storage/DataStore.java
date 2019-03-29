52b2fc896cc90313cc8d6b035905de9f2ef350cc
Kavindra Kulathilake <kkulathilake@gmail.com>
2019-03-30 00:57:13 +0100

    Cache mode changes

--- src/main/java/com/securitybox/storage/DataStore.java
+++ src/main/java/com/securitybox/storage/DataStore.java
@@ -5,10 +5,14 @@ import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
 import org.apache.ignite.IgniteState;
 import org.apache.ignite.Ignition;
+import org.apache.ignite.cache.CacheMode;
+import org.apache.ignite.cache.store.CacheStore;
 import org.apache.ignite.configuration.CacheConfiguration;
+import org.apache.ignite.configuration.IgniteReflectionFactory;
 import org.apache.ignite.transactions.TransactionException;
 import org.json.JSONException;
 
+import javax.cache.configuration.FactoryBuilder;
 import java.util.jar.JarEntry;
 
 import static org.apache.ignite.cache.CacheAtomicityMode.TRANSACTIONAL;
@@ -31,27 +35,16 @@ public  class DataStore implements DataStoreDao{
         CacheConfiguration cfg = new CacheConfiguration();
         cfg.setName(Constants.IGNITE_DEFAULT_CACHE_NAME);
         cfg.setAtomicityMode(TRANSACTIONAL);
+        cfg.setEncryptionEnabled(true);
+        cfg.setCacheMode(CacheMode.LOCAL);
+        IgniteReflectionFactory<CacheStore> storeFactory =
+                new IgniteReflectionFactory(CacheFileLocalStore.class);
+        cfg.setCacheStoreFactory(FactoryBuilder.factoryOf(storeFactory));
         //cache = ignite.getOrCreateCache(cfg);
         objectCache = ignite.getOrCreateCache("CacheEntryObject");
         objectCacheStr = ignite.getOrCreateCache("CacheEntryObject");
     }
 
-
-    //de-tokenize a given token
-    //NOTE : if token is not found in the cache, the same token value returned
-    @Override
-    public String retrieveValue(int token) {
-        String response="";
-        try {
-            response = objectCache.get(Integer.valueOf(token)).getObject().get(Constants.IGNITE_DEFAULT_CACHE_OBJECT_STORE_NAME).toString();
-        } catch (Exception e) {
-            e.getCause();
-            response = Integer.toString(token);
-        }
-        if(response==null)
-            response= Integer.toString(token);
-        return response;
-    }
 
     //Token type String
     @Override
--- src/main/java/com/securitybox/storage/DataStoreDao.java
+++ src/main/java/com/securitybox/storage/DataStoreDao.java
@@ -1,7 +1,6 @@
 package com.securitybox.storage;
 
 public interface DataStoreDao {
-    abstract String retrieveValue(int token);
     abstract  boolean storeValue(int key,CacheEntryObject cacheEntryObject);
     abstract CacheEntryObject retrieveObject(int key);
     abstract boolean storeValue(String key,CacheEntryObject cacheEntryObject);
