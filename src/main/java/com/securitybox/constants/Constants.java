c3ca921f3d785be3e5f8e9b298b5a78b5d2011cd
Kavindra Kulathilake <kkulathilake@gmail.com>
2019-04-03 09:10:03 +0200

    Added constants for file type CSV handling

--- src/main/java/com/securitybox/constants/Constants.java
+++ src/main/java/com/securitybox/constants/Constants.java
@@ -6,9 +6,10 @@ public class Constants {
     public final static String EDIFACT_DATA_ELEMENT_NUMBER = "dataElementNumber";
     public final static String EDIFACT_DATA_ELEMENT_POSITION = "dataElementPosition";
     public final static String EDIFACT_DATA_ELEMENT_LENGTH = "dataElementLength";
-    public static final String DOCUMENT_TYPE_SINGLE_VALUE_TOKEN = "singleValueToken" ;
-    public static final String EDIFACT_SEGMENT_NAME = "segmentName";
-    public static final String EDIFACT_SEGMENT_QUALIFIER = "segmentQualifier";
+    public final static String DOCUMENT_TYPE_SINGLE_VALUE_TOKEN = "singleValueToken" ;
+    public final static String EDIFACT_SEGMENT_NAME = "segmentName";
+    public final static String EDIFACT_SEGMENT_QUALIFIER = "segmentQualifier";
+
     public static String EDIFACT_COMPONENT_DATA_ELEMENT_SEPERATOR=":";
     public static String EDIFACT_DATA_ELEMENT_SEPERATOR="+";
     public static String EDIFACT_SEGMENT_TERMINATOR="'";
@@ -16,6 +17,7 @@ public class Constants {
 
     public final static String DOCUMENT_TYPE_CSV="CSV";
     public final static String CSV_DATA_ELEMENT_POSITION = "dataElementPosition";
+    public final static String CSV_DATA_ELEMENT_LENGTH = "dataElementLength";
 
     public final static String DOCUMENT_TYPE_ALL="ALL";
 
